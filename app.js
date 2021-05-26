2c9c56a0121a4503b55623ce9e25cd74736041ba
eltonwcm00 <eltonwcm00@gmail.com>
2021-05-26 22:50:10 +0800

    express.js redirect & 404

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 // Listen for request
 app.listen(3000);
 
-// Response and request object
 app.get('/', (req, res) => {
    res.sendFile('./views/index.html', { root: __dirname });
 });
@@ -14,3 +13,15 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.sendFile('./views/about.html', { root: __dirname });
 });
+
+// Redirect
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+// 404 Page
+/* Not scope to any particular URL and will fire the function regardless 
+   of the URL, if the code reaches at this point */
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname });
+})
