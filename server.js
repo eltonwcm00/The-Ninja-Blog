4f8f264fe68d1b40ffd2a3230158470737b9ed97
eltonwcm00 <eltonwcm00@gmail.com>
2021-05-26 21:03:35 +0800

    requests & responses

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const http = require('http');
+const fs = require('fs');
 
+/* Response & request object */
 const server = http.createServer((req, res) => {
-    console.log('Request made');
+    
+    // Request
+    console.log(req.url, req.method);
+
+    // Response
+    res.setHeader('Content-Type', 'text/html');
+    
+    // Send an HTML file
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            //res.end(data);
+            res.end();
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {
