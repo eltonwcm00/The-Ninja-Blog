2d7e6f4b581cc9106b6f3838b9faaa8cd2f0418b
eltonwcm00 <eltonwcm00@gmail.com>
2021-05-27 22:25:16 +0800

    server side views rendering

--- app.js
+++ app.js
@@ -10,17 +10,24 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
+    
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+
     // res.sendFile('./views/index.html', { root: __dirname });
-    res.render('index');
+    res.render('index', { title: 'Home', blogs: blogs });
 });
 
 app.get('/about', (req, res) => {
     // res.sendFile('./views/about.html', { root: __dirname });
-    res.render('about');
+    res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create' });
 })
 
 /* 404 Page */
@@ -28,5 +35,5 @@ app.get('/blogs/create', (req, res) => {
 // of the URL, if the code reaches at this point 
 app.use((req, res) => {
     // res.status(404).sendFile('./views/404.html', { root: __dirname });
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404' });
 })
