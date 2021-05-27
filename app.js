2d75c92dc0367836336dc0e1f60fde3fa03e4e0c
eltonwcm00 <eltonwcm00@gmail.com>
2021-05-27 21:55:06 +0800

    .ejs views setup

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 const express = require('express');
 
-// Express app
+/* Express app */
 const app = express();
 
-// Listen for request
+/* Register the view engine with .ejs */
+app.set('view engine', 'ejs');
+
+/* Listen for request */
 app.listen(3000);
 
 app.get('/', (req, res) => {
-   res.sendFile('./views/index.html', { root: __dirname });
+    // res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    // res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
-// Redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+})
 
-// 404 Page
-/* Not scope to any particular URL and will fire the function regardless 
-   of the URL, if the code reaches at this point */
+/* 404 Page */
+// Not scope to any particular URL and will fire the function regardless 
+// of the URL, if the code reaches at this point 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    // res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 })
