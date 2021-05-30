7741804abaf4c1a5c69f4df4b20987ee3a2f99ed
eltonwcm00 <eltonwcm00@gmail.com>
2021-05-30 13:13:22 +0800

    delete req

--- app.js
+++ app.js
@@ -23,11 +23,26 @@ app.use(express.static('public'));
 app.use(express.urlencoded( {extended: true })); // Take all the URL encoded data, pass into an object,that we can use in the reqyest object 
 app.use(morgan('tiny')); // https://www.npmjs.com/package/morgan
 
-app.get('/', (req, res) => {
-    res.redirect('/blogs');
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
 });
 
 /* Basic Routes */ 
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+})
+
+app.get('/about', (req, res) => {
+    // res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
+})
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create' });
+})
+
+/* CRUD */
 // READ : Display all blog routes by cycle through the index.ejs
 app.get('/blogs', (req, res) => {
     Blog.find().sort({createdAt: -1 })
@@ -37,7 +52,7 @@ app.get('/blogs', (req, res) => {
         .catch((err) => {
             console.log(err);
         })
-});
+})
 
 //CREATE : POST request to the server by cycle through the create.ejs
 app.post('/blogs', (req, res) => {
@@ -52,7 +67,7 @@ app.post('/blogs', (req, res) => {
         .catch((err) => {
             console.log(err);
         })
-});
+})
 
 //READ : Read the corresponding id blog post when it is clicked (Route Parameter)
 app.get('/blogs/:id', (req, res) => {
@@ -67,16 +82,23 @@ app.get('/blogs/:id', (req, res) => {
         .catch((err) => {
             console.log(err);
         })
-});
+})
 
-app.get('/about', (req, res) => {
-    // res.sendFile('./views/about.html', { root: __dirname });
-    res.render('about', { title: 'About' });
-});
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create' });
-});
+//DELETE : Delete the corresponding id blog post
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            // Made an AJAX request for deleting, can't use redirect('/blogs).
+            // The request must be made in the front-end not from the server.
+            // Send JSON as the response
+            res.json( {redirect: '/blogs'} );
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
 /* 404 Page */
 app.use((req, res) => {
